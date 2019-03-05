f5df4e12ebe2a638a9c6bd4cb9cde24cacaf00d3
Morris-Trevon <tlove21@gmail.com>
2019-03-05 15:27:08 -0500

    Added CSS transition with JS
    Added CSS transition with JS

--- Scripts/animationtrigger.js
+++ Scripts/animationtrigger.js
@@ -10,5 +10,44 @@ function disruptAnimation(){
 	
 	
 }
+
+
+// Transition Triggers
+
+function playTransition2(){
+	var lambo=document.getElementById('cardrive');
+	
+	if(lambo.classList.contains('zoom2'))
+  {
+    lambo.classList.remove('zoom2');
+	lambo.classList.remove('zoom');
+	//Then jump out and use it
+	 lambo.classList.add('zoom2');
+  } else{
+	
+	lambo.classList.add('zoom2');
+  }
+  
+	return false;
+}
+
+function playTransition(){
+	var lambo=document.getElementById('cardrive');
+	
+	if(lambo.classList.contains('zoom'))
+  {
+    lambo.classList.remove('zoom');
+	lambo.classList.remove('zoom2');
+	//Then jump out and use it
+	lambo.classList.add('zoom');
+  } else{
+	
+	lambo.classList.add('zoom');
+  }
+	
+  
+  return false;
+}
+
 	
 //END
