653033903facceef6b05faf48d548c4a372526d6
Morris-Trevon <tlove21@gmail.com>
2019-04-10 11:51:32 -0500

    Final touches to Final Project

--- Scripts/screenRotation.js
+++ Scripts/screenRotation.js
@@ -1,5 +1,85 @@
 // JavaScript Document
 
+var storage = {};
+
+function submitExplorer() {
+	//Ensure that fresh data is stored by the end of program
+	
+	var dInfo = {};
+	
+	dInfo.fname = document.forms["personnelForm"]["fname"].value;
+	
+	
+	//Validate information entered
+	if (dInfo.fname === "") {
+		alert("All fields must be filled in");
+		return false;
+	 }
+	
+	document.getElementById('output').innerHTML = 'Explorer '+dInfo.fname+" starts their journey!";
+	
+	document.getElementById('homeContent').style.visibility="visible";
+	document.getElementById('homeContent').style.display="block";
+		
+	
+	document.getElementById('personalInformation').style.display="none";
+	
+	
+	dataStorage("explorerInfo",dInfo);
+
+	
+	return false;	
+
+}
+
+function dataStorage(type,getInfo){
+	
+	
+	localStorage.setItem(type,JSON.stringify(getInfo));
+	
+	return false;
+}
+		
+	
+
+function unRavelStorage(){
+	
+	
+	//Unraveling LocalStorage to get information for Driver profile.
+	var getProfile = localStorage.getItem("explorerInfo");
+	var retrievedProf = JSON.parse(getProfile);
+	
+	
+	if(retrievedProf.fname===""){
+		
+		return false;
+		
+	}else{
+	
+	//If successful the values should now be in finalCarOrder
+	document.getElementById('output').innerHTML = '<b>Welcome back Explorer '+retrievedProf.fname+"!</b>";
+	
+	document.getElementById('homeContent').style.visibility="visible";
+	document.getElementById('homeContent').style.display="block";
+		
+	document.getElementById('resetProfile').style.visibility="visible";
+	document.getElementById('resetProfile').style.display="block";
+	
+		
+	document.getElementById('personalInformation').style.display="none";
+	
+		
+		
+	}
+	
+	return false;
+}
+
+function resetProfile(){
+	
+	localStorage.clear();
+	location.reload(); 
+}
 
 function detectClick(catchPage){
 	
